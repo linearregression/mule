632f26cde3fb026a0cbcb1c400fe7c8af4cfd007
JuanDesi <juan.desimoni@mulesoft.com>
2016-11-18 19:54:51 -0300

    MULE-10779: add security strategies to the new WSC

--- extensions/wsc/src/test/java/org/mule/extension/ws/WscUnitTestCase.java
+++ extensions/wsc/src/test/java/org/mule/extension/ws/WscUnitTestCase.java
@@ -6,20 +6,11 @@
  */
 package org.mule.extension.ws;
 
-import static javax.xml.ws.Endpoint.publish;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import org.mule.extension.ws.consumer.SimpleService;
 import org.mule.extension.ws.internal.introspection.WsdlIntrospecter;
 import org.mule.metadata.xml.XmlTypeLoader;
 import org.mule.tck.junit4.rule.DynamicPort;
 
-import javax.xml.ws.Endpoint;
-
-import org.custommonkey.xmlunit.XMLUnit;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
@@ -29,30 +20,18 @@ public abstract class WscUnitTestCase {
   @ClassRule
   public static DynamicPort operationsPort = new DynamicPort("operationsPort");
 
-  public static final String OPERATIONS_URL = "http://localhost:" + operationsPort.getValue() + "/test";
+  @ClassRule
+  public static WebServiceRule service = new WebServiceRule(operationsPort.getValue());
 
   @Rule
   public ExpectedException exception = ExpectedException.none();
 
-  private static Endpoint service;
   protected WsdlIntrospecter introspecter;
   protected XmlTypeLoader loader;
 
-  @BeforeClass
-  public static void startService() {
-    service = publish(OPERATIONS_URL, new SimpleService());
-    assertThat(service.isPublished(), is(true));
-  }
-
   @Before
   public void setup() {
-    XMLUnit.getIgnoreWhitespace();
-    introspecter = new WsdlIntrospecter(OPERATIONS_URL + "?wsdl", "TestService", "TestPort");
+    introspecter = new WsdlIntrospecter(service.getAddress() + "?wsdl", "TestService", "TestPort");
     loader = new XmlTypeLoader(introspecter.getSchemas());
-  }
-
-  @AfterClass
-  public static void shutDownService() {
-    service.stop();
   }
 }
